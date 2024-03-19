29bce2bc266729063ccc16b251418d4132932845
Paul T <paul@pault.com>
2024-03-18 17:48:41 -0700

    Extension first checkin

---
+++ EXT/extension.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = void 0;
+var vscode = require("vscode");
+function activate(context) {
+    var statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+    statusBarItem.text = "$(beaker) Click Me"; // "$(beaker)" will show an icon if available in the current theme
+    statusBarItem.command = "extension.myCommand"; // The command to run when the status bar item is clicked
+    statusBarItem.tooltip = "Click to run command";
+    statusBarItem.show();
+    context.subscriptions.push(statusBarItem);
+    var disposable = vscode.commands.registerCommand('extension.myCommand', function () {
+        vscode.window.showInformationMessage('Button clicked!');
+    });
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+exports.activate = activate;
---
+++ EXT/extension.ts
@@ -0,0 +1,19 @@
+
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+    statusBarItem.text = "$(beaker) Click Me"; // "$(beaker)" will show an icon if available in the current theme
+    statusBarItem.command = "extension.myCommand"; // The command to run when the status bar item is clicked
+    statusBarItem.tooltip = "Click to run command";
+    statusBarItem.show();
+
+    context.subscriptions.push(statusBarItem);
+
+    let disposable = vscode.commands.registerCommand('extension.myCommand', function () {
+        vscode.window.showInformationMessage('Button clicked!');
+    });
+
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
